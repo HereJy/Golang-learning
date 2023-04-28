eeafe492d293ea77f881f37a39b4ce7e14e34bd0
HereJy <h3rjy@protonmail.com>
2023-04-28 08:02:04 +0000

    added modules test

---
+++ test_modules/1.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"test_modules/dir1"
+)
+
+func main() {
+	fmt.Println(dir1.Hello1())
+}
---
+++ test_modules/dir1/dir1.go
@@ -0,0 +1,7 @@
+// dir1.go
+
+package dir1
+
+func Hello1() string {
+	return "Hello everyone, I am from dir 1"
+}
