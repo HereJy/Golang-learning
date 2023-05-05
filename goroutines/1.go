11649314929817f3313c94faebacf26c65d17f43
HereJy <h3rjy@protonmail.com>
2023-05-05 14:44:37 +0200

    updated 1.go in goroutines

--- goroutines/1.go
+++ goroutines/1.go
@@ -6,19 +6,27 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var wg sync.WaitGroup
+
 func main() {
 	debut := time.Now()
+	wg.Add(1)
 	go run("routine 1")
+	wg.Add(1)
 	go run("routine 2")
-	run("routine 3")
+	wg.Add(1)
+	go run("routine 3")
+	wg.Wait() // permet d'attendre la fin des routines avant de reprendre la routine principale
 	fin := time.Now()
 	fmt.Println(fin.Sub(debut))
 }
 
 func run(routine string) {
+	defer wg.Done() // s'execute même en cas de crash grace au defer
 	for i := 0; i < 3; i++ {
 		time.Sleep(1 * time.Second)
 		fmt.Printf("%s : %d\n", routine, i)
